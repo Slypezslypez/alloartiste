ac52ae829393177694f40bfb3ee7d64d37abb0ad
Slypezslypez <157295955+Slypezslypez@users.noreply.github.com>
2026-08-30 00:43:48 +0200

    Update route.ts

--- app/api/artists/me/route.ts
+++ app/api/artists/me/route.ts
@@ -15,7 +15,7 @@ const schema = z
   .object({
     name: z.string().min(2).max(60).optional(),
     category: z.string().min(2).max(60).optional(),
-    specialties: z.array(z.string().max(60)).max(3).optional(),
+    specialties: z.array(z.string().max(60)).max(5).optional(),
     country: z.enum(COUNTRIES).optional(),
     city: z.string().min(1).max(60).optional(),
     bio: z.string().max(600).optional(),
